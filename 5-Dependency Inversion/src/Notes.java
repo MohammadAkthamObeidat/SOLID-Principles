22a232879d8484bbc15764932d3e1608b03ea295
Mohammad Aktham Obeidat <mohamadaktham7@gmail.com>
2021-05-25 20:57:52 +0300

    Some notes about Dependency Inversion Principle

---
+++ 5-Dependency Inversion/src/Notes.java
@@ -0,0 +1,15 @@
+public class Notes {
+
+    /**
+     *                                                        ### Dependency Inversion Definition###
+     *
+     * # Dependency Inversion Principle basically states that High-level modules shouldn't depend on low-level modules. Both are should depend on abstraction.
+     * # Also it says that abstraction shouldn't depends on details. Details should depend on abstraction.
+     */
+
+    /**                                                                 ### Top Down Design ###
+     *
+     * ?? What's top down design??
+     * - Well, when you design your class and you need to implement some business domain flow, you'll usually dedicate a class to it
+     */
+}
